5de084ef6b966a5aec9ea0681f68f87587767007
elifts <coolstuffvideo42@gmail.com>
2023-11-01 17:01:38 +1100

    thrthh

---
+++ code.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Get a reference to the button by its ID
+const myButton = document.getElementById('myButton');
+
+// Add a click event listener to the button
+myButton.addEventListener('click', function () {
+    alert('Button Clicked');
+});
